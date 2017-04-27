841ea8d693696ab1319a5cb11fe6e5c0926ee493
Simon Purup Eskildsen <simonpurup@gmail.com>
2017-04-27 13:49:37 +0200

    Fixed TextInsertEvent with null text

--- src/Project/EventReplayer.java
+++ src/Project/EventReplayer.java
@@ -50,13 +50,15 @@ public class EventReplayer implements Runnable {
 				MyTextEvent mte = dec.take();
 				//If event was not recieved it must be a local event.
 				if(!isRecievedEvent(mte)){
-					HashMap<String, Integer> vectorClock = dte.getVectorClock();
-					eventLog.add(new LoggedEvent(mte,vectorClock, System.nanoTime()));
-					while(eventLog.size() > 0 && System.nanoTime() - eventLog.get(0).time > saveTime){
-						eventLog.remove(0);
+					if(!(mte instanceof TextInsertEvent && ((TextInsertEvent) mte).getText() == null)) {
+						HashMap<String, Integer> vectorClock = dte.getVectorClock();
+						eventLog.add(new LoggedEvent(mte, vectorClock, System.nanoTime()));
+						while (eventLog.size() > 0 && System.nanoTime() - eventLog.get(0).time > saveTime) {
+							eventLog.remove(0);
+						}
+						vectorClock.put(dte.getLocalAddress(), vectorClock.get(dte.getLocalAddress()) + 1);
+						if (connection != null) connection.send(new EventMessage(vectorClock, mte));
 					}
-					vectorClock.put(dte.getLocalAddress(),vectorClock.get(dte.getLocalAddress()) +1);
-				if(connection != null)	connection.send(new EventMessage(vectorClock, mte));
 				}
 			} catch (InterruptedException e) {
 				e.printStackTrace();
